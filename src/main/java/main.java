0627d803ee3cf2f402c6e950f5e5540cd6190ce5
AksaMJML <akaks0186@gmail.com>
2026-01-21 11:21:14 +0530

    created object and set values to the object

---
+++ src/main/java/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setId(1);
+        student.setName("Aksa");
+        student.setAge(28);
+    }
+}
