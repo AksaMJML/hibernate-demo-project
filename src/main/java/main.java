39548ca5170318fdf31e9fcc0189bdbe75a7c5bf
AksaMJML <akaks0186@gmail.com>
2026-01-21 11:45:39 +0530

    This code initializes Hibernate, opens a database session, and saves a student object into the database.

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,8 +1,17 @@
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class main {
     public static void main(String[] args) {
         Student student = new Student();
         student.setId(1);
         student.setName("Aksa");
         student.setAge(28);
+
+        Configuration config = new Configuration();
+        SessionFactory factory = config.buildSessionFactory();
+        Session session = factory.openSession();
+        session.persist(student);
     }
 }
